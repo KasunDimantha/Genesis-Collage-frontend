79e3800d32f04be0be5fe4e5934f342fd75148e1
ChathuVithanage <chathuriimasha60@gmail.com>
2024-05-10 16:40:46 +0530

    updated landing page design

--- GenesisClient/src/App.jsx
+++ GenesisClient/src/App.jsx
@@ -25,7 +25,7 @@ function App() {
         <Routes>
           <Route exact path="/"  element={<Home_Page/>}/>
           <Route exact path='/signUpPage' element={<SignUp_Page/>}/>
-          <Route exact path="/signInPage"   element={!user ? <SignIn_Page/> : 
+          <Route exact path="/signInPage"   element={!user ? < SignIn_Page/> : 
                                                     user.role === "Admin" ? <Navigate to="/a_dashbord"/> :
                                                     user.role === "Student" ? <Navigate to="/s_dashbord"/> :
                                                     <Navigate to="/t_dashbord"/>} />
